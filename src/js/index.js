644dc176c640174505c0b1acaac29102b33bc9ce
Fatbardh <fatbardhemini5@gmail.com>
2020-04-05 22:12:15 -0700

    Final version

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,13 @@
-const hamburger = document.querySelector('.header i');
-const pop = document.querySelector('.pop_list');
-const list = document.querySelector('.pop_items');
-const x = document.querySelector('.pop_list i');
-const body = document.querySelector('body');
-const section = document.querySelector('.header_section');
+var hamburger = document.querySelector('.header i');
+var pop = document.querySelector('.pop_list');
+var list = document.querySelector('.pop_items');
+var x = document.querySelector('.pop_list i');
+var body = document.querySelector('body');
+var section = document.querySelector('.header_section');
 
 hamburger.addEventListener("click", function(){
 	pop.style.width="50%";
-	pop.style.transition = " all 300ms ease-out";
+	pop.style.transition = "all 300ms ease-out";
 	list.style.display = "flex";
 	x.style.display= "block";
 	body.style.overflow = "hidden";
@@ -15,7 +15,7 @@ hamburger.addEventListener("click", function(){
 
 x.addEventListener("click", function(){
 	pop.style.width="0%";
-	pop.style.transition = " all 400ms ease-in";
+	pop.style.transition = "all 400ms ease-in";
 	list.style.display = "none";
 	x.style.display= "none";
 	body.style.overflow = "unset";
